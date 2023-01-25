41835f940144bb969c9abbaf344a7cf63f734cb6
Luka Verzijl <redlucraft@gmail.com>
2023-01-25 12:25:51 +0100

    addded reload btn

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,11 @@
 const homepagebtn = document.getElementById("homepagelink");
 
+const reloadbtn = document.getElementById("startAgain");
+
 homepagebtn.onclick = () => {
     window.location.replace("./index.html")
+}
+
+reloadbtn.onclick = () => {
+    location.reload();
 }
