e19d6aa723774af3260a115cf76688c8c5f0d4b1
stu89605 <117273310+stu89605@users.noreply.github.com>
2023-01-25 14:16:18 +0100

    Update vragenazie.js

--- scripts/vragenazie.js
+++ scripts/vragenazie.js
@@ -1,27 +1,89 @@
 let MCQS = [{
-    question: "vraag",
-    choice1: "x",
-    choice2: "x",
-    choice3: "x",
-    choice4: "x",
-    answer: 1
+    question: "Hoe worden chevrotain ook wel genoemd?",
+    choice1: "Kleine paard",
+    choice2: "Muis hert",
+    choice3: "Kleine muis",
+    choice4: "Kleine ponys",
+    answer: 1 /* Antwoord 2  */
 },
             {
-            question: "vraag",
-    choice1: "x",
-    choice2: "x",
-    choice3: "x",
-    choice4: "x",
-    answer: 2
+            question: "Panda's maken deel uit van welke familie?",
+    choice1: "Beren",
+    choice2: "Wasberen",
+    choice3: "Paarden",
+    choice4: "Apen",
+    answer: 0 /* Antwoord 1  */
 },
-   
+
 {
-            question: "vraag",
-    choice1: "x",
-    choice2: "x",
-    choice3: "x",
-    choice4: "x",
-    answer: 1
+    question: "Wat betekent bedreigd dier?",
+choice1: "Er zijn er veel van dit dier",
+choice2: "Het zijn stinkdieren",
+choice3: "Het zijn dieren die andere dieren opeten",
+choice4: "Er zijn niet veel dieren meer",
+answer: 3 /* Antwoord 4  */
+},
+{
+    question: " Gibbons zijn het kleinste lid van welke familie?",
+choice1: "Aap",
+choice2: "Gorilla's ",
+choice3: "Mormel",
+choice4: "Jenkins",
+answer: 1 /* Antwoord 2  */
+},
+
+{
+    question: " Waar gebruikt de Orangutan bladeren voor?",
+choice1: "Eten",
+choice2: "Voor afkoelen ",
+choice3: "Deken",
+choice4: "WC papier",
+answer: 2 /* Antwoord 3  */
+},
+
+{
+    question: " Waarom zijn de poten van sneeuwluipaarden opgevuld?",
+choice1: "Voor warmte",
+choice2: "Om grote voetafdrukken te maken ",
+choice3: "Om mee te vechten",
+choice4: "Om hun gezicht schoon te maken",
+answer: 2 /* Antwoord 3  */
+},
+
+{
+    question: "Waar jagen tijgers op?",
+choice1: "Leeuwen, Paarden en Olifanten",
+choice2: "Beren, Schapen, Varkens",
+choice3: "Giraffen, Hyenas, Antilope",
+choice4: "Dier, Antilope, Wilde varkens",
+answer: 3 /* Antwoord 4  */
+},
+
+{
+    question: "Hoe sterven dieren uit?",
+choice1: "Prooj",
+choice2: "Gevaar van natuur",
+choice3: "Verloren",
+choice4: "Weers omstandigheden",
+answer: 3 /* Antwoord 4  */
+},
+
+{
+    question: "Met wat voor soort landbouw helpen waterbuffels",
+choice1: "Rijst",
+choice2: "Suikerriet",
+choice3: "Pinda",
+choice4: "Groene bomen",
+answer: 0 /* Antwoord 2  */
+},
+
+{
+            question: "Wat eten panda's?",
+    choice1: "Bladeren",
+    choice2: "Fruit",
+    choice3: "Bamboe",
+    choice4: "Bessen",
+    answer: 2 /* Antwoord 3 */
 
 
 }];
