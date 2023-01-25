492203b0ef0190d4314d04853359ab371f430ded
Luka Verzijl <redlucraft@gmail.com>
2023-01-25 13:12:09 +0100

    moved to seperate quizes

---
+++ scripts/vrageneuropa.js
@@ -0,0 +1,27 @@
+let MCQS = [{
+    question: "vraag",
+    choice1: "x",
+    choice2: "x",
+    choice3: "x",
+    choice4: "x",
+    answer: 1
+},
+            {
+            question: "vraag",
+    choice1: "x",
+    choice2: "x",
+    choice3: "x",
+    choice4: "x",
+    answer: 2
+},
+   
+{
+            question: "vraag",
+    choice1: "x",
+    choice2: "x",
+    choice3: "x",
+    choice4: "x",
+    answer: 1
+
+
+}];
