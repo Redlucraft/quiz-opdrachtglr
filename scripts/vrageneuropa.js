8072c51a82184b9124480f5bca0b0320796b5a3f
Per's Development <pervanhassel@gmail.com>
2023-01-25 14:06:19 +0100

    Quiz europa

--- scripts/vrageneuropa.js
+++ scripts/vrageneuropa.js
@@ -1,27 +1,85 @@
-let MCQS = [{
-    question: "vraag",
-    choice1: "x",
-    choice2: "x",
-    choice3: "x",
-    choice4: "x",
-    answer: 1
-},
-            {
-            question: "vraag",
-    choice1: "x",
-    choice2: "x",
-    choice3: "x",
-    choice4: "x",
-    answer: 2
-},
-   
-{
-            question: "vraag",
-    choice1: "x",
-    choice2: "x",
-    choice3: "x",
-    choice4: "x",
-    answer: 1
+let MCQS = [
+  {
+    question: "Welke soort kikker kan met zijn gif meerdere mensen doden?",
+    choice1: "De kikker",
+    choice2: "De boomkikker",
+    choice3: "De pijlgifkikker",
+    choice4: "De moeraskikker",
+    answer: 2,
+  },
+  {
+    question: "Wat zijn 2 bosdieren?",
+    choice1: "Konijn - Rode mieren",
+    choice2: "Schaap - Kannibalen",
+    choice3: "Koe - Rendier",
+    choice4: "Uil - Varken",
+    answer: 0,
+  },
 
+  {
+    question: "De grootste vis ter wereld?",
+    choice1: "Potvis",
+    choice2: "Walvishaai",
+    choice3: "Orka",
+    choice4: "Dolfijn",
+    answer: 1,
+  },
 
-}];
+  {
+    question: "Hoeveel soorten dieren zijn er ongeveer bekend?",
+    choice1: "65.000",
+    choice2: "120.000",
+    choice3: "1.500.000",
+    choice4: "1.000.000",
+    answer: 2,
+  },
+
+  {
+    question: "Hoeveel verschillende soorten slangen bestaan er?",
+    choice1: "3150",
+    choice2: "3254",
+    choice3: "5013",
+    choice4: "2659",
+    answer: 0,
+  },
+  {
+    question: "Hoe groot kan de Chinese alligator worden?",
+    choice1: "50 centimeter",
+    choice2: "2 Meter",
+    choice3: "1 Meter",
+    choice4: "5 Meter",
+    answer: 1,
+  },
+  {
+    question: "Wat is het kleintje van een paard?",
+    choice1: "Een ram",
+    choice2: "Een kalf",
+    choice3: "Een veulen",
+    choice4: "Een stier",
+    answer: 2,
+  },
+  {
+    question: "Hoe noem je de giftigste spin ter wereld?",
+    choice1: "De braziliaanse zwerfspin",
+    choice2: "Tijgerspin",
+    choice3: "De tarantulla",
+    choice4: "De kruispin",
+    answer: 0,
+  },
+  {
+    question: "Hoeveel kan een mannelijke afrikaanse olifant wegen?",
+    choice1: "500 kg",
+    choice2: "1200 kg",
+    choice3: "8050 kg",
+    choice4: "6000 kg",
+    answer: 3,
+  },
+  {
+    question: "Hoeveel procent van de slangen zijn giftig?",
+    choice1: "51%",
+    choice2: "15%",
+    choice3: "82%",
+    choice4: "21%",
+    answer: 1,
+  },
+];
